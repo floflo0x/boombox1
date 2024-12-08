3d44b9c310118348a5508c3ba959a77425a43b98
floflo0x <148141879+floflo0x@users.noreply.github.com>
2024-12-08 19:50:06 +0530

    Delete middleware directory

--- middleware/is_auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    // console.log(req.session.isLoggedIn, req.cookies._prod_token, req.user?.rem_token);
-    // console.log(req.session.isLoggedIn == 'false' && !req.cookies._prod_token && !req.user?.rem_token);
-    
-    if(req.session.isLoggedIn == 'false' && !req.cookies._prod_token && !req.user?.rem_token) {
-        return res.redirect("/login");
-    }
-    else return next();
-}
